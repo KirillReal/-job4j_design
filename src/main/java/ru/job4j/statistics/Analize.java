c9027e37e41e9a31666d64107ec9084c86901f06
KirillReal <kiril5471710@gmail.com>
2020-12-16 00:59:48 +0300

    2. Статистику по коллекции. [#419280]

---
+++ src/main/java/ru/job4j/statistics/Analize.java
@@ -0,0 +1,78 @@
+package ru.job4j.statistics;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Analize {
+
+    public Info diff(List<User> previous, List<User> current) {
+        Info result = new Info();
+        Map<Integer,String> allUsers = new HashMap<>();
+
+        for (User index: current) {
+            allUsers.put(index.getId(),index.getName());
+        }
+        for(User index: previous) {
+            if(!allUsers.containsKey(index.getId())){
+                result.setDeleted(result.getDeleted() + 1);
+            }else if (!allUsers.get(index.getId()).equals(index.getName())){
+                result.setChanged((result.getChanged() + 1));
+            }
+            allUsers.put(index.getId(), index.getName());
+        }
+        int added = current.size() + result.getDeleted() - previous.size();
+        result.setAdded(result.getAdded() + added);
+        return result;
+    }
+
+    public static class User {
+        int id;
+        String name;
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return this.id;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+    }
+
+
+    public static class Info {
+       private int added = 0;
+       private int changed = 0;
+       private int deleted = 0;
+        public int getAdded() {
+            return added;
+        }
+
+        public int getChanged() {
+            return changed;
+        }
+
+        public void setChanged(int changed) {
+            this.changed = changed;
+        }
+
+        public int getDeleted() {
+            return deleted;
+        }
+
+        public void setDeleted(int deleted) {
+            this.deleted = deleted;
+        }
+
+        public void setAdded(int added) {
+            this.added = added;
+        }
+
+
+
+    }
+}
---
+++ src/test/java/ru/job4j/statistics/AnalizeTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.statistics;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+import org.junit.Test;
+import ru.job4j.statistics.Analize.User;
+import ru.job4j.statistics.Analize.Info;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class AnalizeTest {
+
+    @Test
+    public void diff() {
+        User userPrev1 = new User(1, "One");
+        User userPrev2 = new User(2, "Two");
+        User userPrev3 = new User(3, "Tree");
+
+        List<User> previous =
+                Arrays.asList(userPrev1, userPrev2, userPrev3);
+
+        User userCurr2 = new User(2, "Two");
+        User userCurr3 = new User(3, "Fix");
+        User userCurr4 = new User(4, "Changed");
+
+        List<User> current =
+                Arrays.asList(userCurr2, userCurr3, userCurr4);
+
+        Analize analize = new Analize();
+        Info result = analize.diff(previous, current);
+        assertThat(result.getDeleted(), is(1));
+        assertThat(result.getChanged(), is(1));
+        assertThat(result.getAdded(), is(1));
+    }
+}
