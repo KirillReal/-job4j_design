0149f062f13c401841c89f13c2c6d3a6fb5b9e07
KirillReal <kiril5471710@gmail.com>
2020-12-17 21:19:54 +0300

    0.2. FileInputStream [#419345]

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,24 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+
+        try (FileInputStream inStream = new FileInputStream("even.txt")) {
+            Scanner scanner = new Scanner(System.in);
+            scanner = new Scanner(inStream);
+            TreeSet<Integer> set = new TreeSet<>();
+            while (scanner.hasNext())
+            {
+                int data = scanner.nextInt();
+                if (data % 2 == 0) set.add(data);
+            }
+            for(int i : set) System.out.println(i);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
