d1f85c07880d012f299dd3ae5c37c737a79f3f1a
KirillReal <kiril5471710@gmail.com>
2020-12-20 19:14:43 +0300

    4.1. Сканирование файловой системы. [#419332]

---
+++ src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,24 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+
+public class Search {
+    public static void main(String[] args) throws IOException{
+        if(args.length != 2) {
+            throw new IOException();
+        }
+        Path start = Paths.get(" . ");
+        search(start, "js").forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, String ext) throws IOException {
+        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
+        Files.walkFileTree(root, searcher);
+        return searcher.getPath();
+    }
+}
