cab803c1c754638642529c4e5bbc05267e21c7a9
KirillReal <kiril5471710@gmail.com>
2021-01-04 02:46:55 +0300

    1. Бот [#419358]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.stream.Stream;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -15,18 +16,24 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    String str;
-                    while (!(str = in.readLine()).isEmpty()) {
-                        System.out.println(str);
-                        out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
-                        if(str.equals("http://localhost:9000/?msg=Bye")) {
-                            in.close();
-                            out.close();
-                            socket.close();
-                            server.close();
-                        }
+                    String str = in.readLine();
+                    if(str.equals("http://localhost:9000/?msg=Hello")) {
+                        out.write("Hello".getBytes());
+                    } else if (str.equals("http://localhost:9000/?msg=Exit")) {
+                        in.close();
+                        out.close();
+                        socket.close();
+                        server.close();
+                    }else {
+                        str = Stream.of(str.split(" "))
+                                .filter(s -> s.contains("msg"))
+                                .findFirst()
+                                .orElse("");
+                        System.out.println("msg: " + str);
                     }
-                    
+                    String[] arrStr = str.split("=");
+                    str = arrStr[1];
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".concat(str).getBytes());
                 }
             }
     }
