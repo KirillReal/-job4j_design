f46d00ed88e1bf40b4d820dc07c0457601729bff
KirillReal <kiril5471710@gmail.com>
2021-01-04 03:03:19 +0300

    1. Бот [#419358]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.stream.Stream;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -15,15 +16,22 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    String str;
-                    while (!(str = in.readLine()).isEmpty()) {
-                        System.out.println(str);
-                        out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
-                        if(str.contains("Bye")) {
-                            server.close();
-                        }
+                    String str = in.readLine();
+                    String[] arrStr = str.split("=");
+                    str = arrStr[1];
+                    if(str.contains("Hello")) {
+                        out.write("Hello".getBytes());
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".concat(str).getBytes());
+                    } else if (str.contains("Exit")) {
+                        server.close();
+                    }else {
+                        str = Stream.of(str.split(" "))
+                                .filter(s -> s.contains("msg"))
+                                .findFirst()
+                                .orElse("");
+                        System.out.println("msg: " + str);
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".concat(str).getBytes());
                     }
-
                 }
             }
         }
