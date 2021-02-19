2b14e5c7728a6b816368304b60d47d02223112cb
KirillReal <kiril5471710@gmail.com>
2021-02-20 01:36:37 +0300

    1. Принципы Kiss, Dry и Yagni [#419361]

---
+++ chapter_005/src/main/java/ru/job4j/lambda/MaxMin.java
@@ -0,0 +1,24 @@
+package ru.job4j.lambda;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class MaxMin {
+    public <T> T max(List<T> value, Comparator<T> comparator) {
+        return find(value,comparator);
+    }
+
+    public <T> T min(List<T> value, Comparator<T> comparator) {
+        return find(value, comparator.reversed());
+    }
+
+    private <T> T find(List<T> value,Comparator<T> comparator) {
+        T rsl = value.get(0);
+        for(T t: value) {
+            if(comparator.compare(t,rsl) > 0) {
+                rsl = t;
+            }
+        }
+        return rsl;
+    }
+}
