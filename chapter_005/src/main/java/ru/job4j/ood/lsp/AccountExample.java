a7b9c755e7d4f30b3136d5d59e11b9c70bf3c627
KirillReal <kiril5471710@gmail.com>
2021-02-26 19:04:46 +0300

    0. Видео [#419382]

---
+++ chapter_005/src/main/java/ru/job4j/ood/lsp/AccountExample.java
@@ -0,0 +1,95 @@
+package ru.job4j.ood.lsp;
+
+public class AccountExample {
+    protected int capital;
+
+    public int getCapital() {
+        return capital;
+    }
+
+    public void setCapital(int capital) {
+        this.capital = capital;
+    }
+
+    AccountExample (int sum) throws Exception {
+        if(sum<100)
+            throw new Exception("Некорректная сумма");
+        this.capital = sum;
+    }
+
+    public void SetCapital(int money) throws Exception {
+        if (money < 0)
+            throw new Exception("Нельзя положить на счет меньше 0");
+    }
+
+    public double GetInterest(double sum,  int month, int rate) throws Exception {
+        // предусловие
+        if (sum < 0 || month >12 || month <1 || rate <0)
+            throw new Exception("Некорректные данные");
+
+        double result = sum;
+        for (int i = 0; i < month; i++)
+            result += result * rate  / 100;
+
+        // постусловие
+        if (sum >= 1000)
+            result += 100;
+
+        return result;
+    }
+
+    public int Capital (int value) throws Exception {
+        value = getCapital();
+
+        {
+            if (value < 100)
+                throw new Exception("Некорректная сумма");
+            capital = value;
+        }
+
+        return value;
+    }
+}
+
+class MicroAccount extends AccountExample {
+
+    MicroAccount(int sum) throws Exception {
+        super(sum);
+    }
+
+
+    // Подкласс MicroAccount добавляет дополнительное предусловие, то есть усиливает его, что недопустимо
+    @Override
+    public  void SetCapital(int money) throws Exception {
+        if (money < 0)
+            throw new Exception("Нельзя положить на счет меньше 0");
+
+        if (money > 100)
+            throw new Exception("Нельзя положить на счет больше 100");
+
+    }
+    //Исходя из логики класса Account, в методе CalculateInterest мы ожидаем получить в качестве результата числа 1200.
+    // Однако логика класса MicroAccount показывает другой результат.
+    // В итоге мы приходим к нарушению принципа Лисков - Постусловия не могут быть ослаблены в подклассе
+    @Override
+    public double GetInterest(double sum, int month, int rate) throws Exception {
+        if (sum < 0 || month > 12 || month < 1 || rate < 0)
+            throw new Exception("Некорректные данные");
+
+        double result = sum;
+        for (int i = 0; i < month; i++)
+            result += result * rate /100;
+
+        return result;
+    }
+    //С точки зрения класса Account поле не может быть меньше 100,
+    // и в обоих случаях, где идет присвоение - в конструкторе и свойстве это гарантируется.
+    // А вот производный класс MicroAccount, переопределяя свойство Capital, этого уже не гарантирует.
+    // Поэтому инвариант класса Account нарушается
+    @Override
+    public int Capital (int value){
+        value = getCapital();
+
+        return value;
+    }
+}
